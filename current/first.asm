0eee00d547bc7efda9a685ae5adf23a75d5de09b
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-06-20 21:13:06 +0000

    o kos-silly-loader now properly sets up GDT, A20 address line, protected mode, and   successfully jumps to loaded second sector.  w00t o first.asm now contains 32-bit code with VGA primitive write o Makefile contains some helpful (to me) defaults

--- current/first.asm
+++ current/first.asm
@@ -1,32 +1,16 @@
+[BITS 32]
+[org 0x2000]
+
 start:
-    mov ax, cs
+    mov ax, 10h
     mov ds, ax
-    mov es, ax
+    mov ss, ax
+    mov esp, 090000h
+    mov byte [ds:0B8320h], 'K'
+    mov byte [ds:0B8321h], 1Bh
+    mov byte [ds:0B8322h], 'O'
+    mov byte [ds:0B8323h], 1Bh
+    mov byte [ds:0B8324h], 'S'
+    mov byte [ds:0B8325h], 1Bh
 
-    mov si, msg
-
-    call PrintString
-
-hang:
-    jmp hang            ; just hang around forever
-
-PrintString:
-    ;; use BIOS int 10h 0Eh to print message at ds:si
-    ;;
-    lodsb           ; ds:si copied to al, then increment ds:si
-
-    cmp al, 0       ; are we at null?
-    je .Return      ; ... if so, we're done
-
-    mov ah, 0Eh     ; ... otherwise, set 0xeh mode for int 10h
-    int 10h         ; which prints character at ds:si
-
-    jmp PrintString ; ... and continue to next character
-
-.Return:
-    ret
-
-msg:    db  "The K-OS Loaded", 13, 10, 0  ; message plus CR/LF and NULL
-
-end:
-    TIMES 512-($-$$) DB 0 ; fill to end of containing sector
+    jmp $
--- current/kos-silly-loader.asm
+++ current/kos-silly-loader.asm
@@ -1,61 +1,65 @@
 ; kos-silly-loader
-; version 0.0.3
-; This loader is quite simple.  It creates an IDT, a GDT, set the A20 gate,
-; probes for upper memory, relocates basic BIOS info, loads the next sector
-; off of the disk, then jumps to the new memory location.
+; version 0.0.4
+; This loader is quite simple.  It:
+;   1. sets up a local stack
+;   2. loads 2nd sector off of current disk to fixed memory location
+;   3. configures a base GDT (3 descriptors: null, code, data)
+;   4. sets A20 address line
+;   5. sets cr0 for protected mode
+;   6. far jump to fixed memory location
+; After jump to new kernel, no IDT set, only base GDT set, interrupts are disabled
 ; This boot loader sucks.  Get over it.
 
 [bits 16]
-[org 0]
-zero:
-    jmp 07C0h:start         ; rigorize segment
-
+[org 0x7C00]
 
 ;;
 ;; Reset segment selectors after jumping here to rigorize segment base address
 ;; We do this because the BIOS may jump to any of a number of base addresses,
 ;; as long as we're at 07C0h.  So we perform a jump to a location where 07C0h
-;; is the base address.
+;; is the base address.  (NOTE: we don't do the jump, but might fix that in a bit)
 ;;
+zero:
+
+;;;     jmp 07C0h:start         ; rigorize segment
+;;;     cli         ; disable interrupts
+;;;
+     mov ax, cs
+     mov ds, ax
+     mov es, ax
+
+     sti
+ 
+
 start:
-    cli         ; disable interrupts
-
-    mov ax, cs
-    mov ds, ax
-    mov es, ax
-    
     mov si, boot_msg
-
     call PrintString
 
 
 ;;
-;; Setup a local stack.  BIOS has done so, but using it is purportedly unpredictable
-;; The stack is at 0x9000:0 and is 8192 (0x2000) bytes large
+;; Setup a local stack.  BIOS has done so, but using it is purportedly unpredictable.
+;; The stack is at 0x9000:0 and is 8192 (0x2000) bytes large (grows down)
 ;;
 set_stack:
     mov ax, 0x9000
     mov es, ax
     mov sp, 0x2000
 
-    sti             ; enable interrupts
-
 
 ;;
-;; Using BIOS routines, read adjascent sector into memory (still at 0x1000:0), then jump
-;; to that location
+;; Using BIOS routines, read adjascent sector into memory (to fixed 0x2000)
 ;;
 reset_drive:
     mov dl, 0           ; floppy drive 0
     mov ah, 0           ; int 13h 00h resets drive specified at dl
     int 13h
-    jc reset_failure
+    jc reset_failure    ; if we failed...
 
 load_sector:
-    mov ax, 1000h
-    mov es, ax          ; put data in segment 0x1000
-    xor bx, bx          ; offset 0
-    mov ah, 2           ; int 13h 02h reads sectors from a disk
+    mov ax, 0x200
+    mov es, ax          ; put data 0x2000
+    xor bx, bx          ; offset 0 (so 0x2000:0)
+    mov ah, 2           ; int subcommand: int 13h 02h reads sectors from a disk
     mov al, 1           ; read one sector
     xor ch, ch          ; cylinder 0 (starts at 0)
     mov cl, 2           ; sector 2 (starts at 1)
@@ -64,23 +68,20 @@ load_sector:
 
     int 13h
 
-    jc  failed_one_load ; carry flag == 0 if int 13h 02h successful
-
+    jc  failed_load     ; carry flag == 0 if int 13h 02h successful
     jmp load_successful
 
-failed_one_load:        ; ... otherwise, we failed to load the sector
+
+failed_load:
     mov si, floppy_fail_msg
     jmp reset_drive
 
-cannot_load:
-    mov si, floppy_fail_msg
-    call PrintString
-    jmp $               ; hang forever :(
 
 load_successful:
     mov si, floppy_success_msg
     call PrintString 
-    jmp 1000h:0000
+    jmp create_gdt
+
 
 reset_failure:
     mov si, reset_failure_msg
@@ -89,22 +90,20 @@ reset_failure:
 
 
 ;;
-;; Create Interrupt Descriptor Table (IDT)
-;;
-create_idt:
-
-
-
-;;
-;; Create Global Descriptor Table (GDT)
+;; Create Global Descriptor Table (GDT) -- see below for details
 ;;
 create_gdt:
-    lgdt [gdt_descriptor]
+    xor ax, ax
+    mov ds, ax          ; base for GDT is 0
 
+    lgdt [gdt_ptr]      ; load table pointer
+
+    mov si, gdt_set_msg
+    call PrintString
 
 
 ;;
-;; Enable the A20 gate for full memory access.  Read this for
+;; Enable the A20 address line for full memory access.  Read this for
 ;; gorey details: http://www.osdev.org/wiki/A20_Line.  The code
 ;; is lifted from there.
 ;;
@@ -128,7 +127,7 @@ enable_A20:
         out     0x64,al
 
         call    a20wait
-        pop     ax
+        pop     eax
         or      al,2
         out     0x60,al
 
@@ -138,7 +137,7 @@ enable_A20:
 
         call    a20wait
         sti
-        ret
+        jmp     a20done
 
 a20wait:
         in      al,0x64
@@ -154,15 +153,42 @@ a20wait2:
         ret
 
 
+a20done:
+    mov si, a20_activated_msg
+    call PrintString
+
 
 ;;
 ;; Switch to 32-bit protected mode
 ;;
+to_32bit_pm:
+    ; disable standard interrupts
+    cli
 
+    ; disable IRQs -- it's unclear whether this is strictly necessary
+    mov al, 0xFF
+    out 0x21, al
 
+    ; disable non-maskable interrupts -- it's also unclear if this is strictly necessary
+    ; and actually ... with these two disabled we could miss important hardware interrupts...
+    in al, 0x70
+    or al, 10000000b
+    out 0x70, al
+
+    ; enter protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
 
 ;;
-;; 
+;; Jump to relocated kernel
+;;
+relocate:
+    jmp 08h:2000h   ; REMEMBER: 0x08 is the first byte of the code selector in the GDT
+
+
+
+; ------------------------------------ ;
 
 
 ;; -------------
@@ -191,27 +217,32 @@ PrintString:
 ;; Various Messages for output
 ;;  , 13, 10, 0 is "line feed", "carriage return", "null"
 ;; -------------
-boot_msg:           db  "Silly Bootloader...", 13, 10, 0
+boot_msg:           db  "Silly Bootloader v0.0.3", 13, 10, 0
 
 floppy_fail_msg:    db  "Floppy Read Failure!!", 13, 10, 0
 
 floppy_success_msg: db  "Read First Sector From Floppy", 13, 10, 0
 
-load_success_msg:   db  "(relocate successful)", 13, 10, 0
-
 reset_failure_msg:  db  "reset failed", 13, 10, 0
+
+gdt_set_msg:        db  "GDT Configured", 13, 10, 0
+
+a20_activated_msg:  db  "A20 Address Line Activated", 13, 10, 0
+
+pm32_activated_msg: db  "32-Bit Protected Mode Activated", 13, 10, 0
 
 
 ;; -------------
 ;; Data structures
 ;; -------------
-    ;; Global Descriptor Table length, then address of GDT, will be fed to lgdt instruction
-gdt_descriptor:
-    dw end_gtd_entries - gdt_entries - 1    ; length of GDT
-    dd gdt_entries                          ; start address for GDT
 
-gdt_entries:
-    ;; Global Descriptor Table (http://www.osdever.net/bkerndev/Docs/gdt.htm)
+;; Global Descriptor Table (http://www.osdever.net/bkerndev/Docs/gdt.htm) pointer
+;; and table
+gdt_ptr:
+    dw gdt_table_end - gdt_table_start - 1
+    dd gdt_table_start
+
+gdt_table_start:
     ;; Each entry is 64-bits long, including:
     ;;   16 bits  : limit 
     ;;   24 bits  : base (low/middle)
@@ -228,30 +259,32 @@ gdt_entries:
     ;;               - 1 bit     : size (0 == 16bit; 1 == 32bit)
     ;;               - [remainder are 1]
     ;;    8 bits  : base (high)
-    ;;
-    ;; The first descriptor is the null descriptor, and can have any value
-    ;; but is generally set to all zeros
 
 
-    ; the null descriptor...
+    ;; Required, but not directly used
+    gdt_null:
     dw 0, 0, 0, 0
 
 
-    ; the code segment, contains all of addressable memory, makes this "flat"
-    ; model
+    ;; Entry for "code" section of flat model.  Start point is 0x08
+    gdt_code_entry:
     dw 0xFFFF           ; limit (in 4KiB pages)
     dw 0x0000           ; base low
-    dw 0x009A           ; base middle + present, priv 0, executable, writable
-    dw 0xCF00           ; page granularity, 32bit size + base high 
-    
+    db 0                ; base middle
+    db 10011010b        ; access
+    db 11001111b        ; granularity
+    db 0                ; base high
 
-    ; the data segment, contains all of addressable memory
+
+    ;; Entry for "data" section of flat model.  Start point is 0x10
     dw 0xFFFF           ; limit
     dw 0x0000           ; base low
-    dw 0x0092           ; base middle + present, priv 0, non-exec, readable
-    dw 0xCF00           ; page granularity, 32bit size + base high
+    db 0                ; base middle
+    db 10010010b        ; access
+    db 11001111b        ; granularity
+    db 0                ; base high
 
-end_gtd_entries:    ; used so that NASM can compute the length, in case I change it later
+gdt_table_end:    ; used so that NASM can compute the length, in case I change it later
 
 
 end:
