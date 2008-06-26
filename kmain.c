89d9e679fa74cb6915bf2a55701ca953ca50149c
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-06-26 23:42:15 +0000

    o The VGA library is largely untested, but vga_simple_putc() does work o "kernel" (via kmain()) uses vga_simple_putc() to output 'k-os' at fixed location

--- kmain.c
+++ kmain.c
@@ -1,10 +1,25 @@
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <vga.h>
 
+//void silly_kos( void ) __attribute__((cdecl));
 
 void kmain( void ) {
+//    unsigned short *memptr;
+//    memptr = (unsigned short *)0xB8320;
+//    *memptr = 0x0f00 | (unsigned short)'k';
+//    *memptr++ = 'O' | 0x1b;
+//    *memptr   = 'S' | 0x1b;
 //    vga_simple_putc( 'K', 7, 0 );
 //    vga_simple_putc( 'O', 7, 1 );
 //    vga_simple_putc( 'S', 7, 2 );
+
+//    silly_kos();
+
+    vga_simple_putc( 'k', 5, 0 );
+    vga_simple_putc( '-', 5, 1 );
+    vga_simple_putc( 'o', 5, 2 );
+    vga_simple_putc( 's', 5, 3 );
+
     for (;;) ;
 }
+
