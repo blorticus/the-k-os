665d29927cbe79abf7e9b2791a1dbdb3712b4143
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-06-27 23:46:49 +0000

    o Started update of vga library after failure with use of struct

--- trunk/start.asm
+++ trunk/start.asm
@@ -5,8 +5,13 @@ global start, silly_kos
 start:
 
 segments_set:
+;    mov ax, 08h
+;    mov cs, ax
     mov ax, 10h
     mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
     mov esp, sys_stack
 
@@ -15,18 +20,6 @@ segments_set:
 
     jmp $
 
-silly_kos:
-    push ebx
-
-    mov byte [ds:0B8320h], 'K'
-    mov byte [ds:0B8321h], 1Bh
-    mov byte [ds:0B8322h], 'O'
-    mov byte [ds:0B8323h], 1Bh
-    mov byte [ds:0B8324h], 'S'
-    mov byte [ds:0B8325h], 1Bh
-
-    pop ebx
-    ret
 
 SECTION .bss
     resb 8192
