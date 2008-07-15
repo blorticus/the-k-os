00e7fbaefc7c17393e226bbdb089679de7be3fc8
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-07-15 20:25:16 +0000

    o Back to a working version of the kernel o Now using GRUB to boot (will return to custom bootloader later) o Functioning vga.c, though I'm about to rename it, because I don't believe B8000 memory chunk is VGA

--- start.asm
+++ start.asm
@@ -1,17 +1,39 @@
 [BITS 32]
 
+; On entry, interrupts are disabled
+
+SECTION .text
+
 global start
 
 start:
 
 segments_set:
+    ; setup a small system stack (8kB)
     mov esp, sys_stack
+    jmp kentry
 
+; Multiboot header
+ALIGN 4
+multiboot_header:
+    MULTIBOOT_PAGE_ALIGN    equ 1<<0
+    MULTIBOOT_MEMORY_INFO   equ 1<<1
+    MULTIBOOT_HEADER_MAGIC  equ 0x1badb002
+    MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+    MULTIBOOT_CHECKSUM      equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+kentry:
     extern kmain
     call kmain
 
     jmp $
 
+
+SECTION .data
 
 
 SECTION .bss
