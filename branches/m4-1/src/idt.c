62e16534e3a873ad0de8d450056746ead0cf0a42
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-07-18 23:37:08 +0000

    o Added skeleton for idt.c

---
+++ branches/m4-1/src/idt.c
@@ -0,0 +1,14 @@
+#include <idt.h>
+#include <sys/types.h>
+
+struct idt_table_entry {
+    _U16 base_low_word;
+    _U16 gdt_selector;      // code segment selector in GDT
+    _U8  zeroed_flags;      // for IDT, this byte is always zero
+    _U16 descriptor_flags;  // Present flag + privilege level ("ring") + 0 bit + type
+    _U16 base_high_word;
+} __attribute__((packed));
+
+struct idt_table_entry[256];   // IDT has 256 interrupts.  We don't have to define an entry for all slots, but we'll allocate
+                               // for the whole table
+
