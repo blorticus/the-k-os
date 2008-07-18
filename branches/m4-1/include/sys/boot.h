f91bf93a6b42a654b2376f4c03abae0b323e0b35
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2008-07-18 22:09:42 +0000

    o Moved create_gdt() to separate asm, and now call it in kmain.c (rather than start.asm)

---
+++ branches/m4-1/include/sys/boot.h
@@ -0,0 +1,6 @@
+#ifndef __SYS_BOOT_H__
+#define __SYS_BOOT_H__
+
+void create_gdt( void );
+
+#endif
--- branches/m4-1/kmain.c
+++ branches/m4-1/kmain.c
@@ -1,32 +1,9 @@
 #include <b8000textmode.h>
-
-// 
-// 
-// /**
-//  * Configures a Global Descriptor Table (GDT) with four entries:
-//  *   0x00 - null
-//  *   0x08 - code
-//  *   0x10 - data
-//  *   0x18 - idt
-//  *   0x20 - tss
-//  * Moves CS to 0x08, ds/ss/es/fs/gs to 0x10
-//  **
-// void setup_gdt( void ) {
-// 
-// }
-// 
-// 
-// /**
-//  * Configures an Interrupt Descriptor Table (IDT) with entries for int0h - int10h
-//  * Each of them does nothing for now
-//  */
-// void setup_idt( void ) {
-// 
-// }
+#include <sys/boot.h>
 
 
 void kmain( void ) {
-//    setup_gdt();
+    create_gdt();
 //    setup_idt();
 
     textmode_default_init();
