b658289cc4fc77b9c6cc4f3b5d252ee94aea6a11
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2009-01-02 19:08:30 +0000

    o Started adding memory map to bios output

--- branches/m4-1/kosh/src/kosh.c
+++ branches/m4-1/kosh/src/kosh.c
@@ -38,12 +38,41 @@ kosh_instruction* prompt( void ) {
 void puts_bios_drive_info( u32 drive_info ) {
     u8 drive = (u8)(drive_info >> 24);
 
-    kterm_printf( "drive %x [%s], partition %x:%x:%x",
+    kterm_printf( "drive 0x%x [%s], partition 0x%x:0x%x:0x%x",
                         drive,
                         (drive == 0x00 ? "fd0" : (drive == 0x01 ? "fd1" : (drive == 0x80 ? "hd0" : (drive == 0x81 ? "hd1" : "unknown")))),
                         (u8)(drive_info >> 16),
                         (u8)(drive_info >> 8), 
                         (u8)(drive_info) );
+}
+
+
+/* multiboot_mmap_info->mmap_length is u32, and ->mmap_addr is [base_addr_low, base_addr_high, length_low, length_high, type]
+ * Put the mmap info assuming it follows the literal:
+ * mmap:\n
+ * with one line for each entry
+ */
+typedef struct multiboot_memory_map {
+    u32 size;
+    u32 base_addr_low;
+    u32 base_addr_high;
+    u32 length_low;
+    u32 length_high;
+    u32 type;
+} mb_mmap_t;
+
+
+void puts_multiboot_mmap_info( u32 length, const void* struct_base_addr ) {
+    mb_mmap_t *mmap;
+
+    kterm_printf( "  Map Base Addr = 0x%x, Map Length = %i\n", (u32)struct_base_addr, length );
+
+    for (mmap = (mb_mmap_t*)struct_base_addr;
+         (u32)mmap < (u32)struct_base_addr + length;
+         mmap = ((mb_mmap_t*)(u32)mmap + mmap->size + sizeof(mmap->size))) {
+        kterm_printf( "  - Element:\n    size = %i, base_addr = 0x%x%x, length = 0x%x%x, type = 0x%x\n",
+                mmap->size, mmap->base_addr_high, mmap->base_addr_low, mmap->length_high, mmap->length_low, mmap->type );
+    }
 }
 
 
@@ -109,8 +138,7 @@ int main( void ) {
         /* should not get _ERROR_ or _EMPTY_ from the call to prompt() */
         switch (next_instruction->command) {
             case ECHO:
-                kterm_puts( next_instruction->remaining_command_line );  /* will include inputed newline */
-                kterm_putc( '\n' );
+                kterm_printf( "%s\n", next_instruction->remaining_command_line );  /* will include inputed newline */
                 break;
 
             case PEEK:
@@ -120,49 +148,47 @@ int main( void ) {
                     u8  regval8;
 
                     switch (next_instruction->reg) {
-                        case EAX: M_GR_EAX(regval32); kterm_puts( " eax = " ); kterm_puth( regval32 ); kterm_putc( '\n' ); break;
-                        case AX:  M_GR_AX (regval16); kterm_puts( " ax  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case AL:  M_GR_AL (regval8);  kterm_puts( " al  = " ); kterm_puth( regval8  ); kterm_putc( '\n' ); break;
-                        case AH:  M_GR_AH (regval8);  kterm_puts( " ah  = " ); kterm_puth( regval8  ); kterm_putc( '\n' ); break;
-                        case SS:  M_GR_SS (regval16); kterm_puts( " ss  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case CS:  M_GR_CS (regval16); kterm_puts( " cs  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case DS:  M_GR_DS (regval16); kterm_puts( " ds  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case ES:  M_GR_ES (regval16); kterm_puts( " es  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case FS:  M_GR_FS (regval16); kterm_puts( " fs  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case GS:  M_GR_GS (regval16); kterm_puts( " gs  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
-                        case ESP: M_GR_ESP(regval32); kterm_puts( " esp = " ); kterm_puth( regval32 ); kterm_putc( '\n' ); break;
-                        case SP:  M_GR_SP (regval16); kterm_puts( " sp  = " ); kterm_puth( regval16 ); kterm_putc( '\n' ); break;
+                        case EAX: M_GR_EAX(regval32); kterm_printf( " eax = 0x%x\n", regval32 ); break;
+                        case AX:  M_GR_AX (regval16); kterm_printf( " ax  = 0x%x\n", regval16 ); break;
+                        case AL:  M_GR_AL (regval8);  kterm_printf( " al  = 0x%x\n", regval8  ); break;
+                        case AH:  M_GR_AH (regval8);  kterm_printf( " ah  = 0x%x\n", regval8  ); break;
+                        case SS:  M_GR_SS (regval16); kterm_printf( " ss  = 0x%x\n", regval16 ); break;
+                        case CS:  M_GR_CS (regval16); kterm_printf( " cs  = 0x%x\n", regval16 ); break;
+                        case DS:  M_GR_DS (regval16); kterm_printf( " ds  = 0x%x\n", regval16 ); break;
+                        case ES:  M_GR_ES (regval16); kterm_printf( " es  = 0x%x\n", regval16 ); break;
+                        case FS:  M_GR_FS (regval16); kterm_printf( " fs  = 0x%x\n", regval16 ); break;
+                        case GS:  M_GR_GS (regval16); kterm_printf( " gs  = 0x%x\n", regval16 ); break;
+                        case ESP: M_GR_ESP(regval32); kterm_printf( " esp = 0x%x\n", regval32 ); break;
+                        case SP:  M_GR_SP (regval16); kterm_printf( " sp  = 0x%x\n", regval16 ); break;
 
-                        default : kterm_puts( "Code Incomplete For That Register\n" );
+                        default : kterm_printf( "Code Incomplete For That Register\n" );
                     }
                 }
                 else if (next_instruction->flags & KOSH_INSTRUCTION_FLAG_MEMADDR_SET) {
                     u32 val = *((u32*)(next_instruction->memory_location));
-                    kterm_puts( "addr (" ); kterm_puth( next_instruction->memory_location ); kterm_puts( ") = " ); kterm_puth( val ); kterm_putc( '\n' );
+                    kterm_printf( "addr (0x%x) = 0x%x\n", next_instruction->memory_location, val );
                 }
 
                 break;
 
             case POKE:
-                kterm_puts( "poke\n" );
+                kterm_printf( "poke\n" );
                 break;
 
             case DUMPREGS:
-                kterm_puts( "dumpregs\n" );
+                kterm_printf( "dumpregs\n" );
                 break;
 
             case BIOS:
                 mri = retrieve_multiboot_relocate_info();
+                // for these, really should check mri.flags for each to make sure it is present
                 kterm_printf( "lower mem = %i\n", mri->mem_lower_size_kB );
                 kterm_printf( "upper mem = %i\n", mri->mem_upper_size_kB );
                 kterm_printf( "total mem = %i MB\n", (((mri->mem_lower_size_kB + mri->mem_upper_size_kB) / 1000) + 1) );
                 kterm_printf( "cmdline   = %s\n", mri->cmdline_ptr       );
                 kterm_printf( "bootdev   = " ); puts_bios_drive_info( mri->boot_device ); kterm_printf( "\n" );
+                kterm_printf( "mmap:\n" ); puts_multiboot_mmap_info( mri->mmap_length, mri->mmap_addr ); kterm_printf( "\n" );
 
-//                kterm_puts( "lower mem = " ); kterm_puti( mri->mem_lower_size_kB ); kterm_putc( '\n' );
-//                kterm_puts( "upper mem = " ); kterm_puti( mri->mem_upper_size_kB ); kterm_putc( '\n' );
-//                kterm_puts( "cmdline   = " ); kterm_puts( mri->cmdline_ptr       ); kterm_putc( '\n' );
-//                kterm_puts( "bootdev   = " ); puts_bios_drive_info( mri->boot_device ); kterm_putc( '\n' );
 //    u32 mods_count;
 //    void* mods_addr;
 //    void* syms[4];
