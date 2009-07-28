072aecbc9ce7af8d51a530584f84acec3d419944
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2009-07-28 00:21:47 +0000

    o Added memory map print to bios function of kos

--- trunk/include/multiboot.h
+++ trunk/include/multiboot.h
@@ -3,30 +3,42 @@
 
 #include <sys/types.h>
 
+/* mmap_addr points to mmap_length / size(this) number of these */
+struct multiboot_mmap_entry {
+    u32 entry_size;
+    u32 base_addr_low;
+    u32 base_addr_high;
+    u32 length_low;
+    u32 length_high;
+    u32 type;
+};
+
 /* relocated data by multiboot loader */
 struct multiboot_relocated_info {
-    u32 flags;                  // 0
-    u32 mem_lower_size_kB;      // 4
-    u32 mem_upper_size_kB;      // 8
-    u32 boot_device;            // 12
-    char* cmdline_ptr;          // 16
-    u32 mods_count;             // 20
-    void* mods_addr;            // 24
-    void* syms[4];              // 28
-    u32 mmap_length;            // 44
-    void* mmap_addr;            // 48
-    u32 drives_length;          // 52
-    void* drives_addr;          // 56
-    void* config_table;         // 60
-    char* boot_loader_name;     // 64
-    void* apm_table;            // 68
-    u32 vbe_control_info;       // 72
-    u32 vbe_mode_info;          // 76
-    u16 vbe_mode;               // 80
-    u16 vbe_interface_seg;      // 82
-    u16 vbe_interface_off;      // 84
-    u16 vbe_interface_len;      // 86
+    u32 flags;                              // 0
+    u32 mem_lower_size_kB;                  // 4
+    u32 mem_upper_size_kB;                  // 8
+    u32 boot_device;                        // 12
+    char* cmdline_ptr;                      // 16
+    u32 mods_count;                         // 20
+    void* mods_addr;                        // 24
+    void* syms[4];                          // 28
+    u32 mmap_length;                        // 44
+    struct multiboot_mmap_entry* mmap_addr; // 48
+    u32 drives_length;                      // 52
+    void* drives_addr;                      // 56
+    void* config_table;                     // 60
+    char* boot_loader_name;                 // 64
+    void* apm_table;                        // 68
+    u32 vbe_control_info;                   // 72
+    u32 vbe_mode_info;                      // 76
+    u16 vbe_mode;                           // 80
+    u16 vbe_interface_seg;                  // 82
+    u16 vbe_interface_off;                  // 84
+    u16 vbe_interface_len;                  // 86
 };
+
+
 
 /* immediately after relinquishing control to the OS, a multiboot loader must
  * set EBX to the address of the relocated information struct.  This method
--- trunk/src/stdlib/stdio.c
+++ trunk/src/stdlib/stdio.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <input/keyboard.h>
 
 int getchar( void ) {
     int c;
+    u16 scancodes;
 
     /* grab this right off of the kernel queue.  Obviously, not the right way, but since there is only one process right now... */
     for (;;)
-        if ((c = read_next_key_stroke()))
+        if ((c = read_next_key_stroke( &scancodes )))
             if ((char)c)    /* it is possible to just be a meta character, so we discard those */
                 return (int)((char)c);  /* lower half is the character */
 }
