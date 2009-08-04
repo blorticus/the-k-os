a274c847c9cb72d42f03825d1a5a8986c3977a1b
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2009-08-04 22:18:56 +0000

    o Added kernel memory allocator o Currently, only initialization function is complete o Unit tests in test_kmalloc.c are incomplete

---
+++ trunk/include/kmalloc.h
@@ -0,0 +1,32 @@
+#ifndef __KMALLOC_H__
+#define __KMALLOC_H__
+
+#include <sys/types.h>
+#include <multiboot.h>
+
+/* Initialize structures needed by kmalloc.  Must be called before any blocks allocated
+ * beyond what's defined by the bios memory mapper.  'start_of_mmap' is the pointer to
+ * the first multiboot memory map entry.  'mmap_length' is the length of the map.
+ * 'start_of_kernel' is the start address of the kernel; 'end_of_kernel' is the end address
+ * of the kernel */
+void initialize_kmalloc( const struct multiboot_mmap_entry* start_of_mmap, u32 mmap_length, memaddr start_of_kernel, memaddr end_of_kernel );
+
+/* Allocate block of size 'size' (in bytes) and return pointer to that block.
+ * If no blocks of that size are available, return null */
+void* kmalloc( size_t size );
+
+/* Free allocation referenced by 'ptr'.  If 'ptr' does not reference the start
+ * of an allocated block, do nothing */
+void free( void* ptr );
+
+
+struct free_mem_element {
+    u32 length;
+    struct free_mem_element* prev_element;
+    struct free_mem_element* next_element;
+};
+
+const struct free_mem_element* kmalloc_get_head( void );
+
+
+#endif
