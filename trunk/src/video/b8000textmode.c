baf83497276b3de56560f7c97006ba77d4e83baf
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2009-11-26 00:08:57 +0000

    o Started with b8000 view ports, but I'm going to back out of that and simplify b8000textmode

--- trunk/src/video/b8000textmode.c
+++ trunk/src/video/b8000textmode.c
@@ -12,20 +12,14 @@
     #define WRITE_CHAR_AT(c,ptr) (*ptr = c)
 #endif
 
+
 void textmode_simple_putc( char c, u16 row, u16 col ) {
     u16* memptr;
 
     memptr = (u16 *)TEXTMODE_BASE_MEM + row * 80 + col;
-//    *memptr = 0x0f00 | (u16)c;
     WRITE_CHAR_AT( (0x0f00 | (u16)c), memptr );
 }
 
-
-//u8 textmode_color;
-//u8 textmode_width;
-//u8 textmode_height;
-//u8 textmode_current_row;
-//u8 textmode_current_col;
 
 u16* textmode_memptr;
 
@@ -34,20 +28,43 @@ u16* textmode_memptr;
 static b8000_textmode_viewport root_vp;
 
 
-static void textmode_scroll_vp( b8000_textmode_viewport* vp ) {
+b8000_textmode_viewport* retrieve_default_viewport( void ) {
+    return &root_vp;
+}
+
+
+
+void textmode_configure_simple_viewport( b8000_textmode_viewport* vp, u8 start_row, u8 end_row, u8 current_row, u8 current_col ) {
+    if (start_row >= end_row || current_row < start_row || current_row > end_row)
+        return;
+
+    vp->start_col   = 0;
+    vp->end_col     = root_vp.end_col;
+    vp->width       = root_vp.width;
+    vp->start_row   = start_row;
+    vp->end_row     = end_row;
+    vp->height      = end_row - start_row;
+    vp->current_row = current_row;;
+    vp->current_col = current_col;
+    vp->color       = root_vp.color;
+}
+
+
+void textmode_multi_scroll_vp( b8000_textmode_viewport* vp, u8 scroll_rows ) {
+    if (scroll_rows == 0)
+        return;
+
     u16 i;
 
-    u16* dst_ptr = (u16 *)TEXTMODE_BASE_MEM;
-    u16* src_ptr = (u16 *)TEXTMODE_BASE_MEM + vp->width;
+    u16* dst_ptr = (u16 *)TEXTMODE_BASE_MEM + (vp->width * vp->start_row);
+    u16* src_ptr = (u16 *)TEXTMODE_BASE_MEM + (vp->width * vp->start_row + vp->width * scroll_rows);
 
     /* inefficient 2byte-by-2byte copy */
     /* first, copy every row to the row above it, except the first row */
-//    for (i = 0; i < (textmode_height - 1) * textmode_width; i++)
-    for (i = vp->start_row; i <= vp->end_row * vp->width; i++)
+    for (i = vp->start_row + scroll_rows - 1; i <= vp->end_row * vp->width; i++)
         *dst_ptr++ = *src_ptr++;
 
     /* then, fill in the last row with spaces to clear any content that was there */
-//    for (i = 0; i < textmode_width; i++)
     for (i = vp->start_col; i <= vp->end_col; i++)
         *dst_ptr++ = ' ' | TEXTMODE_ATTR(vp);
 
@@ -57,17 +74,12 @@ static void textmode_scroll_vp( b8000_textmode_viewport* vp ) {
 }
 
 
-//static void textmode_scroll( void ) {
-//    textmode_scroll_vp( &root_vp );
-//}
+void textmode_scroll_vp( b8000_textmode_viewport* vp ) {
+    textmode_multi_scroll_vp( vp, 1 );
+}
+
 
 void textmode_init( u8 width, u8 height, u8 at_row, u8 at_col, u8 bgcolor, u8 fgcolor ) {
-//    textmode_width        = width;
-//    textmode_height       = height;
-//    textmode_current_row  = at_row;
-//    textmode_current_col  = at_col;
-//    textmode_color        = (u8)((bgcolor << 4) | fgcolor);
-
     root_vp.start_col       = 0;
     root_vp.end_col         = width - 1;
     root_vp.width           = width;
@@ -90,12 +102,11 @@ void textmode_init( u8 width, u8 height, u8 at_row, u8 at_col, u8 bgcolor, u8 fg
 
     /*
      * The VGA I/O port 0x0a address activates/deactivates cursor (bit 5) and the cursor start register.
-     * We inactivate the cursor because it's kind of annoying to track by simply setting the entire 0x0a
-     * I/O port data to 1's
-     * XXX: THIS DOESN'T WORK AND NEEDS MORE RESEARCH
+     * We inactivate the cursor because it's kind of annoying to track by simply setting bit 5 to 0, the
+     * unused bits 6 and 7 to 0, and all the rest of the bits to 1
      */
     ioport_writeb( 0x3d4, 0x0a );
-    ioport_writeb( 0x3d5, 0x0f );
+    ioport_writeb( 0x3d5, 0x1f );
 }
 
 
@@ -104,13 +115,10 @@ void textmode_init_default( void ) {
 }
 
 void textmode_set_location_vp( b8000_textmode_viewport* vp, u8 row, u8 column ) {
-//    if (row >= textmode_height || column >= textmode_width)
     if (row > vp->end_row || column > vp->end_col)
         return;
 
-//    textmode_current_row = row;
     vp->current_row = row;
-//    textmode_current_col = column;
     vp->current_col = column;
 }
 
@@ -128,60 +136,42 @@ int textmode_putchar( int c ) {
 
 void textmode_putc_vp( b8000_textmode_viewport* vp, char c ) {
     if (c == '\r') {
-//        textmode_current_col = 0;
         vp->current_col = 0;
     }
     else if (c == '\n') {
-//        textmode_current_col = 0;
-//        textmode_current_row++;
         vp->current_col = 0;
         vp->current_row++;
     }
     else if (c == '\t') {
-//        textmode_current_col += 4;
         vp->current_col += 4;
     }
     else if (c == '\b') {
-//        if (textmode_current_col == 0) {
         if (vp->current_col == vp->start_col) {
-//            if (textmode_current_row > vp.start_row) {
             if (vp->current_row > vp->start_row) {
-//                textmode_current_row--;
                 vp->current_row--;
-//                textmode_current_col = textmode_width - 1;
                 vp->current_col = vp->end_col;
-//                textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (textmode_current_row * textmode_width) + textmode_current_col;
                 textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (vp->current_row * vp->width) + vp->current_col;
                 *textmode_memptr = ' ' | TEXTMODE_ATTR(vp);
             }
         }
         else {
-//            textmode_current_col--;
             vp->current_col--;
-//            textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (textmode_current_row * textmode_width) + textmode_current_col;
             textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (vp->current_row * vp->width) + vp->current_col;
             *textmode_memptr = ' ' | TEXTMODE_ATTR(vp);
         }
     }
     else if (c >= ' ') {
-//       textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (textmode_current_row * textmode_width) + textmode_current_col;
        textmode_memptr = (u16 *)TEXTMODE_BASE_MEM + (vp->current_row * vp->width) + vp->current_col;
        *textmode_memptr = c | TEXTMODE_ATTR(vp);
 
-//        if (++textmode_current_col >= textmode_width) {
         if (++vp->current_col > vp->end_col) {
-//            textmode_current_row++;
             vp->current_row++;
-//            textmode_current_col = 0;
             vp->current_col = 0;
         }
     }
  
-//    if (textmode_current_row >= textmode_height) {
     if (vp->current_row > vp->end_row) {
-//        textmode_scroll();
         textmode_scroll_vp( vp );
-//        textmode_current_row = textmode_height - 1;
         vp->current_row = vp->end_row;
     }
 }
@@ -194,25 +184,18 @@ void textmode_putc( char c ) {
 
 
 void textmode_putc_at_vp( b8000_textmode_viewport* vp, char c, u8 row, u8 col ) {
-//    if (row >= textmode_height || col >= textmode_width)
     if (row > vp->end_row || col > vp->end_col)
         return;
 
-//    u8 hold_row = textmode_current_row;
     u8 hold_row = vp->current_row;
-//    u8 hold_col = textmode_current_col;
     u8 hold_col = vp->current_col;
 
-//    textmode_current_row = row;
     vp->current_row = row;
-//    textmode_current_col = col;
     vp->current_col = col;
 
     textmode_putc( c );
 
-//    textmode_current_row = hold_row;
     vp->current_row = hold_row;
-//    textmode_current_col = hold_col;
     vp->current_col = hold_col;
 }
 
@@ -224,7 +207,6 @@ void textmode_putc_at( char c, u8 row, u8 col ) {
 
 void textmode_puts_vp( b8000_textmode_viewport* vp, char *s ) {
     while (*s)
-//        textmode_putc( *s++ );
         textmode_putc_vp( vp, *s++ );
 }
 
@@ -235,26 +217,18 @@ void textmode_puts( char *s ) {
 
 
 void textmode_puts_at_vp( b8000_textmode_viewport* vp, char* s, u8 row, u8 col ) {
-//    if (row >= textmode_height || col >= textmode_width)
     if (row > vp->end_row || col > vp->end_col)
         return;
 
-//    u8 hold_row = textmode_current_row;
     u8 hold_row = vp->current_row;
-//    u8 hold_col = textmode_current_col;
     u8 hold_col = vp->current_col;
 
-//    textmode_current_row = row;
     vp->current_row = row;
-//    textmode_current_col = col;
     vp->current_col = col;
 
-//    textmode_puts( s );
     textmode_puts_vp( vp, s );
 
-//    textmode_current_row = hold_row;
     vp->current_row = hold_row;
-//    textmode_current_col = hold_col;
     vp->current_col = hold_col;
 }
 
@@ -280,7 +254,6 @@ void textmode_puti_vp( b8000_textmode_viewport* vp, unsigned int i ) {
             i = i / 10;
         }
 
-//    textmode_puts( (char*)(int_buff + last_digit) );
     textmode_puts_vp( vp, (char*)(int_buff + last_digit) );
 }
 
@@ -292,25 +265,18 @@ void textmode_puti( unsigned int i ) {
 
 
 void textmode_puti_at_vp( b8000_textmode_viewport* vp, unsigned int i, u8 row, u8 col ) {
-//    if (row >= textmode_height || col >= textmode_width)
     if (vp->current_row > vp->end_row || vp->current_col > vp->end_col)
         return;
 
-//    u8 hold_row = textmode_current_row;
     u8 hold_row = vp->current_row;
-//    u8 hold_col = textmode_current_col;
     u8 hold_col = vp->current_col;
 
-//    textmode_current_row = row;
     vp->current_row = row;
-//    textmode_current_col = col;
     vp->current_col = col;
 
     textmode_puti( i );
 
-//    textmode_current_row = hold_row;
     vp->current_row = hold_row;
-//    textmode_current_col = hold_col;
     vp->current_col = hold_col;
 }
 
@@ -328,13 +294,10 @@ void textmode_cls_vp( b8000_textmode_viewport* vp ) {
 
     textmode_memptr = (u16 *)TEXTMODE_BASE_MEM;
 
-//    for (i = 0; i < textmode_height * textmode_width; i++)
     for (i = vp->start_row; i <= vp->end_row * vp->width; i++)
         *textmode_memptr++ = ' ' | TEXTMODE_ATTR(vp);
 
-//    textmode_current_col = 0;
     vp->current_col = 0;
-//    textmode_current_row = 0;
     vp->current_row = vp->start_row;
 }
 
