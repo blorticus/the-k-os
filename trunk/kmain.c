7d6747e1c561c164c0296680a3473cde637e0266
blort <blort@caabff9f-1d7f-70a9-9c5b-60ef93edad06>
2010-02-24 20:42:51 +0000

    o Reverted b8000textmode to pre-viewport, then added "windowing" which is essentially the same   as viewport, but the implementation itself is cleaner

--- trunk/kmain.c
+++ trunk/kmain.c
@@ -1,4 +1,5 @@
 #include <video/b8000textmode.h>
+#include <video/kterm.h>
 #include <sys/boot.h>
 #include <sys/asm.h>
 #include <idt.h>
@@ -8,6 +9,13 @@
 #include <multiboot.h>
 
 int main( void );
+
+B8000_TEXTMODE_WINDOW w;
+//B8000_TEXTMODE_WINDOW w2;
+//int i;
+//void _write_char_at( char c, u16* offset );
+
+char buf[80];
 
 void kmain( void ) {
     multiboot_relocate();
@@ -23,8 +31,70 @@ void kmain( void ) {
     // MUST happen after texmode initalization since isrs/irqs currently may use this mode
     enable_interrupts();
 
-//    textmode_cls();
-//    textmode_puts( "The K-OS is now loaded.\n\n\n" );
+    textmode_cls();
+    textmode_puts( "The K-OS is now loaded.\n\n\n" );
+
+//  void create_textmode_window( B8000_TEXTMODE_WINDOW* w, u8 start_row, u8 window_height, u8 window_width, u8 colors ) {
+
+//    init_textmode_window( &w, 1, 5, 80, 0x0f );
+//    init_textmode_window( &w2, 7, 4, 80, 0x0f );
+//    textmode_window_cls( &w );
+
+//    textmode_window_puts( &w, "LINE 1\nLINE 2\nLINE 3\n" );
+//    textmode_window_puts( &w, "LINE 1" );
+
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'A' );
+//
+//    textmode_window_cls( &w2 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, 'a' );
+// 
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'B' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, 'b' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'C' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, 'c' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'D' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, 'd' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'E' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, 'e' );
+//
+//    kterm_readline( &buf, 80 );
+//    textmode_window_cls( &w );
+//
+//    kterm_readline( &buf, 80 );
+//    textmode_window_cls( &w2 );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w, 'A' );
+//
+//    kterm_readline( &buf, 80 );
+//    for (i = 0; i < 80; i++)
+//        textmode_window_putc( &w2, '1' );
 
     main();  /* call into KoSH */
 
