492b3bee8d9c23d1961f3f6ec6a5818da3bbf764
blorticus <vlists@marybrynn.org>
2018-01-19 22:52:23 -0800

    Switched to using gcc builtin va_args for lprintf

--- src/stdlib/cprintf.c
+++ src/stdlib/cprintf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdarg.h>
 
 /* Given a particular number base (currently 10 and 16 are supported), convert 'value' into its ascii numeric representation.
  * Place the acsii representation in 'buf', which will be null terminated.  If 'max_len' greater than characters needed to
@@ -232,89 +232,14 @@ int cprintf( void (*putchar_f)(int, ...), char* putchar_args, const char *fmt, .
 }
 
 
-typedef struct {
-    uint64 first_non_fp;
-    uint64 second_non_fp;
-    uint64 third_non_fp;
-    uint64 fourth_non_fp;
-    uint64 fifth_non_fp;
-    uint64 sixth_non_fp;
-
-    double first_fp;
-    double second_fp;
-    double third_fp;
-    double fourth_fp;
-    double fifth_fp;
-    double sixth_fp;
-    double seventh_fp;
-    double eighth_fp;
-} t_saved_varargs;
-
-static __attribute__((always_inline)) inline uint64 vargs_get_next_non_fp_arg( t_saved_varargs* va, int int_va_count ) {
-    switch (int_va_count) {
-        case 1: return va->first_non_fp; break;
-        case 2: return va->second_non_fp; break;
-        case 3: return va->third_non_fp; break;
-        case 4: return va->fourth_non_fp; break;
-        case 5: return va->fifth_non_fp; break;
-        case 6: return va->sixth_non_fp; break;
-        default:
-            return 0;
-            break;
-    }
-}
-
-static __attribute__((always_inline)) inline uint64 vargs_get_next_fp_arg( t_saved_varargs* va, int fp_va_count ) {
-    switch (fp_va_count) {
-        case 1: return va->first_fp; break;
-        case 2: return va->second_fp; break;
-        case 3: return va->third_fp; break;
-        case 4: return va->fourth_fp; break;
-        case 5: return va->fifth_fp; break;
-        case 6: return va->sixth_fp; break;
-        case 7: return va->seventh_fp; break;
-        case 8: return va->eighth_fp; break;
-        default:
-            return 0.0;
-            break;
-    }
-}
-
 int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t *fmt, ... ) {
     char_t wcbuf[20];
 
-    uint64 rax;
-    t_saved_varargs va;
-
-    asm __volatile__ ("movq %%rcx, %0\n\t"
-                      "movq %%r8, %1\n\t"
-                      "movq %%r9, %2"
-                      : "=r"(va.fourth_non_fp), "=r"(va.fifth_non_fp), "=r"(va.sixth_non_fp)
-                      :
-                      );
-
-    asm __volatile__ ("movq %%rax, %0" : "=r"(rax) : );
-
-    if (rax > 0) {
-        asm __volatile__ ("movq %%xmm0, %0\n\t"
-                          "movq %%xmm1, %1\n\t"
-                          "movq %%xmm2, %2\n\t"
-                          "movq %%xmm3, %3\n\t"
-                          "movq %%xmm4, %4\n\t"
-                          "movq %%xmm5, %5\n\t"
-                          "movq %%xmm6, %6\n\t"
-                          "movq %%xmm7, %7"
-                          : "=r"(va.first_fp), "=r"(va.second_fp), "=r"(va.third_fp), "=r"(va.fourth_fp),
-                            "=r"(va.fifth_fp), "=r"(va.sixth_fp), "=r"(va.seventh_fp), "=r"(va.eighth_fp)
-                          :
-                         );
-    }
+    va_list ap;
 
     unsigned int printed_chars = 0;
 
     const char_t* p = fmt;
-
-    int next_non_fp_arg_num = 4;
 
     long l;
     char c;
@@ -322,6 +247,8 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
     int padding;
     const char* s;
     const char_t* w;
+
+    va_start(ap, fmt);
 
     while (*p) {
         padding = 0;
@@ -339,8 +266,7 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
 
                 case 'i':
                 case 'd':
-                    //l = (int)*next_stack_arg++;
-                    l = vargs_get_next_non_fp_arg( &va, next_non_fp_arg_num++ );
+                    l = va_arg(ap, sint64);
                     w = wcbuf;
                     count = wctoi( wcbuf, l, 10, 19 );
 
@@ -352,8 +278,7 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
                     break;
 
                 case 'x':
-                    //l = (long)*next_stack_arg++;
-                    l = vargs_get_next_non_fp_arg( &va, next_non_fp_arg_num++ );
+                    l = va_arg(ap, sint64);
                     w = wcbuf;
                     count = wctoi( wcbuf, l, 16, 19 );
 
@@ -365,14 +290,13 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
                     break;
 
                 case 'c':
-                    //c = *((char*)next_vararg);
-                    c = (char)(vargs_get_next_non_fp_arg( &va, next_non_fp_arg_num++ ));
+                    c = (char)va_arg(ap, int);
                     putwchar_f( putwchar_f_arg, c );
                     printed_chars++;
                     break;
 
                 case 's':
-                    s = (char*)(vargs_get_next_non_fp_arg( &va, next_non_fp_arg_num++ ));
+                    s = va_arg(ap, char*);
 
                     while (padding-- > 0) {
                         putwchar_f( putwchar_f_arg, ' ' );
@@ -387,7 +311,7 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
                     break;
 
                 case 'w':
-                    w = (char_t*)(vargs_get_next_non_fp_arg( &va, next_non_fp_arg_num++ ));
+                    w = va_arg(ap, char_t*);
 
                     print_wchar_string:
                          while (padding-- > 0) {
@@ -419,6 +343,8 @@ int lprintf( void (*putwchar_f)(void*, int), void* putwchar_f_arg, const char_t 
             p++;  // advance beyond format char
         }
     }
+
+    va_end(ap);
 
     return printed_chars;
 }
